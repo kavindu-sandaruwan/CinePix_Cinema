e490476009217b85503a2d29d2ba08a4a7e3d0f1
Kavindu Sandaruwan <kavinduvishmithasandaruwan@gmail.com>
2023-01-14 04:16:04 +0530

    Updated Table

--- CinePix_Cinema_Kvs/CinePix_Cinema/web/DB/movies.sql
+++ CinePix_Cinema_Kvs/CinePix_Cinema/web/DB/movies.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 13, 2023 at 11:37 PM
+-- Generation Time: Jan 13, 2023 at 11:44 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
