c305d5b2deb247cfdc7dc48045f44d9058e37026
Vinuri Disara <vinuridisara2020@gmail.com>
2023-01-16 13:06:28 +0530

    sending email files

---
+++ CinePix_Cinema_Kvs/CinePix_Cinema/src/java/email.java
@@ -0,0 +1,35 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = {"/email"})
+public class email extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String movie = request.getParameter("moviename");
+        String email = request.getParameter("email");
+        String date = request.getParameter("date");
+        String message = request.getParameter("message");
+        String subject = "Online Ticket Reservation";
+
+        String message1 = "Thank you " + message + " This is your reserve id " + date;
+
+        request.setAttribute("email", email);
+        request.setAttribute("subject", subject);
+        request.setAttribute("message", message1);
+
+        RequestDispatcher rd = request.getRequestDispatcher("sendingEmailReceipt.jsp");
+        rd.forward(request, response);
+
+    }
+
+}
